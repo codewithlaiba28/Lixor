b55edbf070d19d537c6e75eb835000f4b2399e02
codewithlaiba28 <codewithlaiba28@gmail.com>
2026-05-01 18:42:25 +0500

    fix: Webpack alias to stub onnxruntime-node for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@xenova/transformers', 'onnxruntime-node'],
+  serverExternalPackages: ['@xenova/transformers'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "onnxruntime-node$": false,
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
